1ab14fe8cf19b8f580c3f6487ae6df0ca50d235b
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-11-20 15:33:03 -0300

    Add PATCH method for restaurants

--- src/main/java/com/example/algafoodapi/api/controller/RestauranteControlller.java
+++ src/main/java/com/example/algafoodapi/api/controller/RestauranteControlller.java
@@ -7,13 +7,18 @@ import com.example.algafoodapi.domain.model.Estado;
 import com.example.algafoodapi.domain.model.Restaurante;
 import com.example.algafoodapi.domain.repository.RestauranteRepository;
 import com.example.algafoodapi.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 /*** Indico que essa classe é uma Controller que atenderá requisicoes e tambem que suas respostas irão para o corpo da requisicao*/
@@ -83,7 +88,31 @@ public class RestauranteControlller {
         }
     }
 
-    
+    @PatchMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+        Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+        if(restauranteAtual==null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        merge(campos, restauranteAtual);
+
+        return atualizar(restauranteId, restauranteAtual);
+    }
+
+    private  void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+        ObjectMapper objectMapper = new ObjectMapper(); // Classe do jackson que faz a conversão da classe para json e vice versa
+        Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class); //Converte o json insirido no corpo da requisicao na classe Restaurante
+        //Faz as conversoes necessarias para nao precisar ficar fazendo checagem e conversao
+
+        camposOrigem.forEach((String key, Object value) -> {
+            Field field = ReflectionUtils.findField(Restaurante.class, key); //Busca as propriedades da classe em questão e me dá acesso a elas em tempo de execução
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+            field.setAccessible(true); //Faço isso para acessar a propriedade que é privada
+//            ReflectionUtils.setField(field, restauranteDestino, value);
+            ReflectionUtils.setField(field, restauranteDestino, novoValor);
+        });
+    }
 
 
 }
